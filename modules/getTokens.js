ca2837465b349dc3dddb32c2a1506026024ad74c
Filipe Melo Silva <filipemelodasilva@gmail.com>
2015-05-18 23:20:41 -0300

    Criação de defereds para promessas

--- modules/getTokens.js
+++ modules/getTokens.js
@@ -1,32 +1,42 @@
 var querystring = require('querystring');
+var Q = require('Q');
 
-var getTokens = function(item, http) {
+var getTokens = function(item, http, deferred) {
     console.log('Get Token for API:', item.apiName);
+
     var token, headers;
     var postData = querystring.stringify(item.authenticateParams);
+    var optionsRequest = item.authenticateRules;
+    optionsRequest.port = 80;
 
     if (item.authenticateRules.method.toLowerCase() === 'post'){
         headers = {
             "Content-Type" : "application/x-www-form-urlencoded",
-            "Content-Length" : postData.length
+            "Content-Length" : Buffer.byteLength(postData)
         };
     } else {
         headers = ''
     }
 
-    var optionsRequest = item.authenticateRules;
     optionsRequest.headers = headers;
 
-    var postReq = http.request(optionsRequest, function(resp){
-        resp.setEncoding('utf8');
+    var postRequest = Q.nfcall(http.request, [optionsRequest] );
+console.log(postRequest);
+    // var postRequest = http.request(optionsRequest, function(resp){
+    //     resp.setEncoding('utf8');
 
-        resp.on('data', function (chunk) {
-            console.log('Response: ' + chunk);
-        });
-    });
+    //     resp.on('data', function (chunk) {
+    //         deferred.resolve();
+    //         console.log('Response: ' + chunk);
+    //     });
+    // });
 
-    postReq.write(postData);
-    postReq.end();
+//console.log(deferred);
+
+    postRequest.write(postData);
+    postRequest.end();
+
+    return deferred.promisse;
 };
 
 module.exports = getTokens;
--- server.js
+++ server.js
@@ -6,14 +6,22 @@ var apiList = require('./data/api-list.json');
 
 http.createServer(function (req, res) {
     console.log('Request Start');
-    var result;
+    var promissesListResult  = [] ;
 
     apiList.forEach(function(item) {
-        item.token = getApiTokens(item, http);
+        var deferred = Q.defer();
+
+        var result = getApiTokens(item, http, deferred);
+
+        console.log(result);
     });
-    
 
     console.log('Request End');
 }).listen(1337, '127.0.0.1');
+
+
+process.on('uncaughtException', function (err) {
+    console.log(err);
+}); 
 
 console.log('Server running at http://127.0.0.1:1337/');
