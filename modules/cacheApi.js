54780a0d8d22f25af1f57329d1fb195512017a3c
Filipe Melo Silva <filipemelodasilva@gmail.com>
2015-05-31 15:48:52 -0300

    Criação do objeto para cache

---
+++ modules/cacheApi.js
@@ -0,0 +1,23 @@
+﻿var timer = require('timers');
+
+var cacheList = {
+    objs : [],
+    getCached : function(apiName) {
+        var data;
+
+        for (var i = 0; i <  this.objs.length; i++) {
+            var cachedData = this.objs[i];
+            if (cachedData && cachedData.name === apiName.toLowerCase) {
+                data = cachedData;
+            }
+        };
+
+        return data;
+    },
+    setCache : function(apiObj) {
+        this.objs.push(apiObj);
+    },
+    setCacheTimer : function(key, time) {
+
+    }
+};
