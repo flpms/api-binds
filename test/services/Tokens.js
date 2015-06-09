c8435950fcd01db29711c6c73925a8c7bfc13d65
Filipe Melo Silva <filipemelodasilva@gmail.com>
2015-06-08 21:46:25 -0300

    Revert "Adição de testes com API do Apontador"
    This reverts commit aeeb8c2d525c5c39d3717d45a7ea018acabd2a3a.

--- test/services/Tokens.js
+++ test/services/Tokens.js
@@ -3,23 +3,23 @@ var getTokens = require('../modules/getTokens.js');
 
 //mock data
 var data = {
-    "name" : "apontador",
-    "authenticateRules" : {
-        "host" : "api.apontador.com.br",
-        "path" : "/v2/oauth/token",
-        "method" : "POST"
-    },
-    "authenticateParams" : {
-        "client_id" : "monitoring-reviews",
-        "client_secret" : "mDdRQOEXcVTfX80X8X0H0ct3NXs~",
-        "grant_type" : "client_credentials",
-        "scope": "read"
-    },
-    "expirationTime" : ""
-};
+        "name" : "apontador",
+        "authenticateRules" : {
+            "host" : "api.apontador.com.br",
+            "path" : "/v2/oauth/token",
+            "method" : "POST"
+        },
+        "authenticateParams" : {
+            "client_id" : "monitoring-reviews",
+            "client_secret" : "mDdRQOEXcVTfX80X8X0H0ct3NXs~",
+            "grant_type" : "client_credentials",
+            "scope": "read"
+        },
+        "expirationTime" : ""
+    };
 
 describe('Test for Tokens', function() {
 	it('Should return a Object for new Instance', function() {
-        var 
+
 	});
 });
